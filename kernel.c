5571ea334a738ce920999cec71c88e82a89006ca
achance6 <ayden.chance@rocketmail.com>
2024-12-08 16:54:30 -0500

    removed commented code

--- kernel.c
+++ kernel.c
@@ -3,16 +3,6 @@
 #include "keyboard.h"
 
 void main() {
-  /* clear_terminal(); */
-  /* print_string("HELLO"); */
-  /* print_line("WORLD"); */
-  /* print_string("TODAY"); */
-  /* print_character('c'); */
-  /* print_character('\n'); */
-  /* print_string_with_color("hello", YELLOW, CYAN); */
-  /* print_line_with_color("World", MAGENTA, GREEN); */
-  /* print_string("Today"); */
-
   clear_terminal();
   uint8_t byte;
 
